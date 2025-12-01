e121e19783a0d54d63250a4fb98c91cdc773a142
VergeOne <info@verge-one.com>
2025-12-01 21:04:58 +0100

    Initial Commit with bug

---
+++ proxy.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  const session = (() => null)();
+  console.log("Middleware session:", session);
+  if (!session) {
+    console.log("Redirecting to /invoice/signin");
+    const signInUrl = request.nextUrl.clone();
+    signInUrl.pathname = "/signin";
+
+    return NextResponse.redirect(signInUrl);
+  }
+  return NextResponse.next();
+}
+export const config = {
+  //   matcher: "/", // WORKS, REDIRECTS TO /invoice/signin
+  matcher: ["/((?!signin|signup|api|_next/static|_next/image).*)"], // DOES NOT WORK!!!
+};
